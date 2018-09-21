9ca0a5d69b53a014bc1bb3a69022785d31b1deac
RodrigoCruz1154 <00115417@uca.edu.sv>
2018-09-21 09:30:09 -0600

    siguiente

---
+++ ejercicio06.js
@@ -0,0 +1,11 @@
+var insertar = prompt("Inserte un número: ");
+function abs(num){
+    if(num<0){
+        num = num * -1;
+        return num;
+    }
+    else{
+        return num;
+    }
+}
+abs(insertar);
